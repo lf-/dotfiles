e6049e3185355e1701c261e6aaa5519b0355db6e
Cyril Plisko <cyril.plisko@mountall.com>
2016-05-09 23:38:48 +0300

    API query works

---
+++ src/crates.rs
@@ -0,0 +1,86 @@
+use std::collections::HashMap;
+
+#[derive(Debug, Default, Deserialize)]
+pub struct Error {
+    pub detail: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CrateLinks {
+    pub owners: String,
+    pub reverse_dependencies: String,
+    pub version_downloads: String,
+    pub versions: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Crate {
+    pub created_at: String,
+    pub description: Option<String>,
+    pub documentation: Option<String>,
+    pub downloads: u64,
+    pub homepage: Option<String>,
+    pub id: String,
+    pub keywords: Vec<String>,
+    pub license: String,
+    pub links: CrateLinks,
+    pub max_version: String,
+    pub name: String,
+    pub repository: String,
+    pub updated_at: String,
+    pub versions: Vec<u64>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Keyword {
+    pub crates_cnt: u64,
+    pub created_at: String,
+    pub id: String,
+    pub keyword: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct VersionLinks {
+    pub authors: String,
+    pub dependencies: String,
+    pub version_downloads: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Version {
+    #[serde(rename="crate")]
+    pub krate: String,
+    pub created_at: String,
+    pub dl_path: String,
+    pub downloads: u64,
+    pub features: HashMap<String, Vec<String>>,
+    pub id: u64,
+    pub links: VersionLinks,
+    pub num: String,
+    pub updated_at: String,
+    pub yanked: bool,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Reply {
+    #[serde(default)]
+    pub errors: Error,
+    #[serde(rename="crate")]
+    pub krate: Crate,
+    pub keywords: Vec<Keyword>,
+    pub versions: Vec<Version>,
+}
+
+pub type CratesReply = HashMap<String, String>;
+
+#[derive(Deserialize, Debug)]
+struct GenericResponse {
+    args: HashMap<String, String>,
+    data: Option<String>,
+    files: Option<HashMap<String, String>>,
+    form: Option<HashMap<String, String>>,
+    headers: HashMap<String, String>,
+    json: Option<String>,
+    origin: String,
+    url: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+#![cfg_attr(feature="clippy", feature(plugin))]
+#![cfg_attr(feature="clippy", plugin(clippy))]
+#![feature(custom_derive, plugin)]
+#![plugin(serde_macros)]
+
+#[macro_use]
+extern crate clap;
+extern crate requests;
+extern crate serde;
+extern crate serde_json;
+
+use std::fmt;
+use clap::{App, SubCommand, AppSettings};
+mod crates;
+
+const CARGO: &'static str = "cargo";
+
+fn report(krate: crates::Reply) {
+
+}
+
+fn query(krate: &str) -> Option<crates::Reply> {
+    let response = requests::get(&format!("http://crates.io/api/v1/crates/{}", krate)).unwrap();
+    //println!("{:?}", response.json().unwrap());
+    let reply = response.from_json::<crates::Reply>().unwrap();
+    println!("{:?}", reply.krate);
+    Some(reply)
+}
+
+fn debug<T>(item: &T) where T: fmt::Debug {
+    println!("{:?}", item);
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let matches = App::new(CARGO)
+                      .bin_name(CARGO)
+                      //.author(crate_author!())
+                      .version(crate_version!())
+                      .about("Querys registry for crates details")
+                      .setting(AppSettings::GlobalVersion)
+                      .setting(AppSettings::VersionlessSubcommands)
+                      .setting(AppSettings::SubcommandRequired)
+                      .setting(AppSettings::ArgRequiredElseHelp)
+                      .subcommand(SubCommand::with_name("info")
+                                      .setting(AppSettings::ArgRequiredElseHelp)
+                                      .setting(AppSettings::TrailingVarArg)
+                                      .arg_from_usage("<crate>... 'crate to query'"))
+                      .get_matches();
+
+    if let Some(crates) = matches.subcommand_matches("info")
+                                  .and_then(|m| m.values_of("crate")) {
+
+        for krate in crates {
+            debug(&krate);
+            query(krate);
+        }
+    }
 }
