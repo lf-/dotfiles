ee657dacec0ba8021675c08dd1ce1db1add0783f
Cyril Plisko <cyril.plisko@mountall.com>
2016-09-04 17:00:03 +0300

    Add brief version history in the default output
    Closes #13

--- src/crates.rs
+++ src/crates.rs
@@ -253,8 +253,16 @@ impl fmt::Display for Crate {
                    format_args!("{:<16}{}  ({:#})", "Created at:", created_at, created_at),
                    format_args!("{:<16}{}  ({:#})", "Updated at:", updated_at, updated_at))
         } else {
+            let mut versions = String::new();
+            for line in self.print_last_versions(5, false).lines() {
+                versions = versions + "\n";
+                if !line.is_empty() {
+                    versions = versions + "  " + line;
+                }
+            }
+
             write!(f,
-                   "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}",
+                   "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}",
                    format_args!("{:<16}{}", "Crate:", self.krate["name"]),
                    format_args!("{:<16}{}", "Version:", self.krate["max_version"]),
                    format_args!("{:<16}{}", "Description:", self.krate["description"]),
@@ -262,7 +270,8 @@ impl fmt::Display for Crate {
                    format_args!("{:<16}{}", "Homepage:", self.krate["homepage"]),
                    format_args!("{:<16}{}", "Documentation:", self.krate["documentation"]),
                    format_args!("{:<16}{}", "Repository:", self.krate["repository"]),
-                   format_args!("{:<16}{:#}", "Last updated:", updated_at))
+                   format_args!("{:<16}{:#}", "Last updated:", updated_at),
+                   format_args!("{:<16}\n{}", "Version history:", versions))
         }
     }
 }
