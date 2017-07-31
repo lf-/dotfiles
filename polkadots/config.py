056dbb67bb1b5e19aca8bc8a98d23e7b190e49ba
lf- <lf-@users.noreply.github.com>
2017-07-31 13:20:16 -0600

    Switch to new polkadots config format

---
+++ polkadots/config.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+dotconfig = lambda n: (n, (Path('~/.config') / n))
+
+actions = [
+    SymlinkAction('colors', '~/.colors'),
+    SymlinkAction('qt4/Trolltech.conf', '~/.config/Trolltech.conf'),
+    SymlinkAction('gtk3', '~/.config/gtk-3.0'),
+    SymlinkAction('prezto', '~/.zprezto'),
+    SymlinkAction('icons', '~/.icons'),
+    SymlinkAction('bin', '~/bin'),
+    SymlinkAction('fonts', '~/.fonts'),
+    SymlinkAction('main', '~', dir_mode=True),
+]
+
+dotconfigs = [
+    'polkadots',
+    'aiopanel',
+    'sxhkd',
+    'nvim',
+    'bspwm',
+    'qemu-box'
+]
+
+for dc in dotconfigs:
+    actions.append(SymlinkAction(*dotconfig(dc)))
