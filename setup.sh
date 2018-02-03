a5f8fbac2b49670bd99bac045dd23d993f54f274
lf- <lf-@users.noreply.github.com>
2018-02-03 05:37:38 +0000

    Make setup.sh work with new neovim config location

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ fi
 
 # Installer for vimrc and plugins
 #git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-curl -fLo ~/.nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall
