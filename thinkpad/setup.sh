67ab76b60449de9586e33539945bd5289be9fae7
lf- <lf-@users.noreply.github.com>
2018-04-17 20:42:07 -0600

    Remember to actually make the directory

--- thinkpad/setup.sh
+++ thinkpad/setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Setup script for ThinkPads
 
+set -e
+
 if [[ $EUID != 0 ]]; then
 	echo 'Here: you lost this: sudo' >&2
 	exit 1
 fi
 
+mkdir -p /etc/X11/xinitrc.d
 cp xinitrc.d/* /etc/X11/xinitrc.d
 cp tlp /etc/default
