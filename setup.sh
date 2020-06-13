cda9b59117ed34b90c4b6ad4f8913d78f314f26f
lf- <lf-@users.noreply.github.com>
2020-06-12 23:03:54 -0700

    Add python

--- setup.sh
+++ setup.sh
@@ -10,3 +10,4 @@ fi
 #git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall
+echo 'Run `:CocInstall coc-python coc-rls coc-json`.
