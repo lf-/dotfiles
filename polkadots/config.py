0e0a3eb6d4985616989090f31f8bb19292ac28d5
lf- <lf-@users.noreply.github.com>
2021-02-17 03:50:38 -0800

    Slight code crime to have diff font size per machine

--- polkadots/config.py
+++ polkadots/config.py
@@ -1,4 +1,35 @@
 from pathlib import Path
+import subprocess
+import re
+import logging
+
+LAPTOP_THRESHOLD = 75000
+
+def get_is_laptop():
+    # XXX: this is mildly cursed
+    try:
+        randr_output = subprocess.check_output('xrandr').decode()
+    except subprocess.CalledProcessError:
+        logging.warning('xrandr was not available or failed. defaulting to '
+                        'desktop alacritty config')
+        randr_output = ''
+
+    sq_mm_re = re.compile(r'^.+?(\d+(\.\d+)?)mm x (\d+(\.\d+)?)mm$')
+    primary_sq_mm = None
+    for line in randr_output.split('\n'):
+        if 'primary' in line:
+            m = sq_mm_re.match(line)
+            if not m:
+                logging.warning('failed to match xrandr output')
+                continue
+            h, _, w, _ = m.groups()
+            if not h or not w:
+                logging.warning('xrandr output changed??')
+                continue
+            primary_sq_mm = int(h) * int(w)
+    return primary_sq_mm and primary_sq_mm < LAPTOP_THRESHOLD
+
+
 dotconfig = lambda n: (n, (Path('~/.config') / n))
 
 actions = [
@@ -21,5 +52,16 @@ dotconfigs = [
     'alacritty',
 ]
 
+alacritty_config = 'alacritty-laptop.yml' \
+    if get_is_laptop() \
+    else 'alacritty-desktop.yml'
+
 for dc in dotconfigs:
     actions.append(SymlinkAction(*dotconfig(dc)))
+
+alacritty = Path('~/.config/alacritty')
+actions.append(CatAction(
+    alacritty / 'alacritty.yml',
+    alacritty / 'alacritty-base.yml',
+    alacritty / alacritty_config
+))
