6a17f9e8b9c5024baf50f7b2f0d7784b294a7c54
Jade Lovelace <jadel@mercury.com>
2022-06-01 16:10:15 -0700

    use fancyindex because we can

--- docs-svc/docs-service.nix
+++ docs-svc/docs-service.nix
@@ -1,4 +1,7 @@
 let pkgs = import <nixpkgs> {};
+  nginx = pkgs.nginx.override {
+    modules = pkgs.nginx.modules ++ [pkgs.nginxModules.fancyindex];
+  };
 in
   pkgs.runCommandLocal "docs-service" { } ''
   mkdir -p $out
@@ -6,10 +9,10 @@ in
     ${./docs.service} \
     $out/docs.service \
     --replace /usr/bin/mkdir ${pkgs.coreutils}/bin/mkdir \
-    --replace /usr/bin/nginx ${pkgs.nginx}/bin/nginx \
+    --replace /usr/bin/nginx ${nginx}/bin/nginx \
     --replace nginx.conf $out/nginx.conf
   substitute \
     ${./nginx.conf} \
     $out/nginx.conf \
-    --replace /etc/nginx ${pkgs.nginx}/conf
+    --replace /etc/nginx ${nginx}/conf
   ''
