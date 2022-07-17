8ae98a30ce4a3eb9f7d4967b28cd2c885c7f4c72
Jade Lovelace <software@lfcode.ca>
2022-07-16 20:22:57 -0700

    vim-jq

--- configs/nvim/lua/plugins.lua
+++ configs/nvim/lua/plugins.lua
@@ -74,6 +74,7 @@ return require('packer').startup(function(use)
     use 'qnighy/lalrpop.vim'
     use 'editorconfig/editorconfig-vim'
     use 'tikhomirov/vim-glsl'
+    use 'bfrg/vim-jq'
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
