4b24292a0189d490fbb7b83687990b81b1b117b8
Jade Lovelace <software@lfcode.ca>
2022-07-16 20:30:54 -0700

    bootloader, hmm

--- configs/nix/machines/snowflake/default.nix
+++ configs/nix/machines/snowflake/default.nix
@@ -1,9 +1,13 @@
 { config, lib, nixpkgs, ... }: {
-  imports = [ ../../roles/dev ../../roles/linux ../../roles/users ];
+  imports = [ ../../roles/dev ../../roles/linux ../../roles/users ./hardware-configuration.nix ];
 
   boot.initrd.availableKernelModules = [ "aesni_intel" "cryptd" ];
   # create a swap file on the encrypted partition
   swapDevices = [ { device = "/swapfile"; size = 16384; } ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
