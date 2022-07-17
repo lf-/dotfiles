d7468b5d5fa00c073ed7625acd1785d7b5eb8596
Jade Lovelace <software@lfcode.ca>
2022-07-16 20:27:44 -0700

    hm

--- configs/nix/machines/snowflake/default.nix
+++ configs/nix/machines/snowflake/default.nix
@@ -1,7 +1,15 @@
 { config, lib, nixpkgs, ... }: {
-  imports = [ ../../roles/dev ../../roles/linux ];
+  imports = [ ../../roles/dev ../../roles/linux ../../roles/users ];
 
   boot.initrd.availableKernelModules = [ "aesni_intel" "cryptd" ];
   # create a swap file on the encrypted partition
   swapDevices = [ { device = "/swapfile"; size = 16384; } ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
 }
