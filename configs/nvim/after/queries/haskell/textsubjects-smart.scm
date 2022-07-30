794d0dc9222a4e18591a3955e70aac9a42c59c88
Jade Lovelace <software@lfcode.ca>
2022-07-29 18:56:23 -0700

    Initial go at a textsubjects for haskell

---
+++ configs/nvim/after/queries/haskell/textsubjects-smart.scm
@@ -0,0 +1,104 @@
+([
+  (exp_do)
+  (stmt)
+  (exp_cond)
+  (exp_tuple)
+  (exp_list)
+  (patterns)
+  (guard)
+
+  (signature)
+  (function)
+
+  (data_constructor)
+  (record_fields)
+
+  (newtype)
+  (instance)
+  (adt)
+  (top_splice)
+ ] @_start @_end
+ (#make-range! "range" @_start @_end))
+
+; guard equation rhs
+; startOfThisWeek day | dayOfWeek day == Monday
+;    = day
+((guard_equation
+   (guards)
+   "="
+   (_) @_start @_end)
+ (#make-range! "range" @_start @_end))
+
+; signature + function
+(
+ ((signature) @_start
+  .
+  (function) @_end)
+ (#make-range! "range" @_start @_end))
+
+; function apply param
+(
+ (exp_apply . (_) (_) @_start @_end)
+ (#make-range! "range" @_start @_end))
+
+; function definition lhs
+((function
+    pattern: (_) @_start @_end)
+ (#make-range! "range" @_start @_end))
+
+; function definition rhs (this matters if it is not a do)
+((function . (_)+
+    rhs: (_) @_start @_end)
+ (#make-range! "range" @_start @_end)
+)
+
+; let-in rhs
+(
+ ; test =
+ ;     let
+ ;         a = 1
+ ;         b = 2
+ ;     in a + b
+ (exp_let_in (exp_let) (exp_in "in" (_) @_start @_end))
+ (#make-range! "range" @_start @_end))
+
+; one record field
+((
+  ; data A = A
+  ; { fieldOne :: Int
+  ; , fieldTwo :: Int
+  ; }
+  (field (_)) @_start @_end . (comma)? @_end
+ )
+ (#make-range! "range" @_start @_end))
+
+([
+  ; lhs of a bind_pattern assignment
+  ; MkTy {tyOne, tyTwo} <- undefined
+  (bind_pattern . (_) @_start @_end)
+  ; rhs
+  (bind_pattern (_) @_start @_end .)
+ ]
+ (#make-range! "range" @_start @_end))
+
+(
+ ; parts of a signature
+ ; someFunc :: forall a. (a -> String)
+ [
+  ; lhs
+  (signature
+   name: (_) @_start @_end
+  )
+  ; rhs
+  (signature
+    type: "::"
+    type: (_) @_start @_end
+  )
+ ]
+ (#make-range! "range" @_start @_end)
+)
+
+(((_) @head . (comment) @_start . (comment)+ @_end (_) @tail)
+    (#not-has-type? @tail "comment")
+    (#not-has-type? @head "comment")
+    (#make-range! "range" @_start @_end))
