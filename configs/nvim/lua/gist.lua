f3cdd8fcaaf8d53723443b9d5e629fe730a0cb0f
Jade Lovelace <jadel@mercury.com>
2022-08-15 17:11:36 -0700

    there we go

--- configs/nvim/lua/gist.lua
+++ configs/nvim/lua/gist.lua
@@ -3,9 +3,11 @@ local Job = require('plenary.job')
 function do_gist(args)
     local lines = vim.api.nvim_buf_get_lines(0, args.line1 - 1, args.line2 - 1, true)
     local text = (table.concat(lines, '\n'))
+    local filename = vim.fn.expand('%')
+
     local job = Job:new {
         command = 'gh',
-        args = {'gist', 'create'},
+        args = {'gist', 'create', '-f', filename},
         on_exit = function (j, return_val)
             if return_val ~= 0 then
                 print('gist failed')
@@ -14,7 +16,7 @@ function do_gist(args)
             local result = j:result()[1]
 
             -- TODO: this doesnt work for some weird unknown reason
-            vim.schedule_wrap(function ()
+            vim.schedule(function ()
                 print('copied', result)
                 vim.fn.setreg('+', result)
             end)
@@ -28,6 +30,6 @@ end
 vim.api.nvim_create_user_command('Gist', (function (args)
     do_gist(args)
 end), {
-    desc = "Gists the current file with gh gist",
+    desc = "Gists the current file privately with gh gist",
     range = '%'
 })
