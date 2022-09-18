a01c59a82a6ac9ad40a11544e3f6c995a011798e
Jade Lovelace <software@lfcode.ca>
2022-09-17 18:02:49 -0700

    delete obsolescent

--- configs/nix/hls.nix
+++
@@ -1,8 +0,0 @@
-# haskell language server
-let
-  sources = import ./nix/sources.nix;
-  # hls = import sources.haskell-language-server;
-  hls = import ~/dev/haskell-language-server;
-  pkgs = import hls.inputs.nixpkgs {};
-  inherit (pkgs.haskell.lib) appendConfigureFlag;
-in appendConfigureFlag hls.defaultPackage."${pkgs.system}" "-frename"
