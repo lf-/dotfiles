63590295dfbab71575d838ffe3b075c6e7bc2704
Jade Lovelace <software@lfcode.ca>
2022-12-03 22:36:20 -0800

    Make the textsubjects smart selection work better in nix

--- configs/nvim/after/queries/nix/textsubjects-smart.scm
+++ configs/nvim/after/queries/nix/textsubjects-smart.scm
@@ -1,4 +1,10 @@
 ;; extends
-((function_expression) @_start @_end
+
+([(indented_string_expression)
+  (function_expression)
+  (list_expression)
+  (binding)
+  (attrset_expression)] @_start @_end
  (#make-range! "range" @_start @_end)
 )
+
