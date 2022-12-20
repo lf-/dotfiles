e16932bc0856dd62aef543ac59346410c3ff0373
Jade Lovelace <software@lfcode.ca>
2022-12-19 22:02:22 -0800

    some treesitter

---
+++ configs/nvim/after/queries/rust/textsubjects-smart.scm
@@ -0,0 +1,4 @@
+;; extends
+
+((let_declaration) @_start @_end
+ (#make-range! "range" @_start @_end))
--- configs/nvim/lateinit.lua
+++ configs/nvim/lateinit.lua
@@ -83,6 +83,11 @@ require 'nvim-treesitter.configs'.setup {
 require 'treesitter-context'.setup {
     max_lines = 3,
     min_window_height = 30,
+    patterns = {
+        nix = {
+            'binding'
+        }
+    }
 }
 
 require('telescope').setup {
