38a29c11d64b7fe4ead5c4e0d99c138faae05eb7
Jade Lovelace <software@lfcode.ca>
2023-09-26 14:42:59 -0700

    attempt jpdftweak by just grabbing binaries

--- configs/nix/packages/jpdftweak/package.nix
+++ configs/nix/packages/jpdftweak/package.nix
@@ -1,92 +1,29 @@
-{ stdenv, lib, fetchzip, jdk, ant, fetchurl, p7zip, fetchMavenArtifact }:
-let
-  jmupdf = stdenv.mkDerivation {
-    pname = "jmupdf";
-    version = "0.4.1";
-
-    src = fetchurl {
-      name = "source";
-      url = "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/jmupdf/2012-02-23-source-jmupdf-0.4.1.7z";
-      hash = "sha256-x/11RQlWaL2Nz6TYdKCOFbqIPDmFO6DfNIRsoD4uSI0=";
-
-      downloadToTemp = true;
-
-      postFetch = ''
-        ${p7zip}/bin/7z x -o$out $downloadedFile
-      '';
-
-      recursiveHash = true;
-    };
-
-    nativeBuildInputs = [ jdk ant ];
-
-    prePatch = ''
-      substituteInPlace jmupdf/jmupdf/build.xml --replace 'value="''${basedir}/bin"' 'value="''${basedir}/src"'
-    '';
-
-    buildPhase = ''
-      cd jmupdf/jmupdf
-      ant
-    '';
-
-    installPhase = ''
-      mkdir -p $out
-      cp build/jmupdf.jar $out
-    '';
-  };
-
-  fetchMavenArtifactOrig = fetchMavenArtifact;
-  deps =
-    let
-      fetchMavenArtifact = args: { jarName = args.jarName or args.artifactId; jar = (fetchMavenArtifactOrig (builtins.removeAttrs args [ "jarName" ])).passthru.jar; };
-    in
-    [
-      (fetchMavenArtifact {
-        jarName = "itext";
-        groupId = "com.itextpdf";
-        artifactId = "itextpdf";
-        version = "5.1.3";
-        hash = "sha256-Y0UJU+SVUTFABwY/9/bhgK9E0qepqzG2m46xnql+dkY=";
-      })
-      (fetchMavenArtifact {
-        groupId = "com.jgoodies";
-        artifactId = "jgoodies-common";
-        version = "1.4.0";
-        hash = "sha256-79mG7IUcOlzVeQfnLyfRr5weWxh0sHqLd4WIZG1x2fw=";
-      })
-      (fetchMavenArtifact {
-        jarName = "forms";
-        groupId = "com.jgoodies";
-        artifactId = "jgoodies-forms";
-        version = "1.6.0";
-        hash = "sha256-9IKrYZ0n0CyiXSB/B3DrnguSlYYi65cQXJXStIZIqMw=";
-      })
-    ];
-in
+{ stdenv, lib, fetchzip, jdk, ant, fetchurl, p7zip, jre, fetchMavenArtifact, runtimeShell }:
 stdenv.mkDerivation {
   pname = "jpdftweak";
   version = "1.1";
 
-  passthru.deps = deps;
-  passthru.jmupdf = jmupdf;
-
   src = fetchzip {
-    url = "http://prdownloads.sourceforge.net/jpdftweak/jpdftweak-src-1.1.zip";
+    url = "http://prdownloads.sourceforge.net/jpdftweak/jpdftweak-1.1.zip";
     stripRoot = false;
-    sha256 = "sha256-Fby6D97NR8hq/cLdcgdDr63Z6rTxzlttTomWYzEpxIc=";
+    sha256 = "sha256-rQv3HH1lXaOSldJjDUTRZEbdXamlZC/HpSwk0KvUPsc=";
   };
 
-  prePatch = ''
-    sed -i -e 's/source="1.5"/source="7"/g' -e 's/target="1.5"/target="7"/g' build.xml
-    ${lib.concatMapStringsSep "\n" (artifact: ''
-      cp ${artifact.jar} lib/${artifact.jarName}.jar
-    '') deps}
+  buildPhase = "true";
+
+  installPhase = ''
+    mkdir -p $out
+    mkdir -p $out/share/jpdftweak/java
+    cp lib/* $out/share/jpdftweak/java
+    mkdir -p $out/share/java
+    cp jpdftweak.jar $out/share/java
+
+    mkdir -p $out/bin
+    cat > $out/bin/jpdftweak <<EOF
+    #!${runtimeShell}
+    export JAVA=${jre}/bin/java
+    JAVA_HOME=${jre} \$JAVA -cp $out/share/jpdftweak/java -jar $out/share/java/jpdftweak.jar
+    EOF
+    chmod +x $out/bin/jpdftweak
   '';
-
-  nativeBuildInputs = [
-    jdk
-    ant
-  ];
-
-  buildPhase = "ant";
 }
