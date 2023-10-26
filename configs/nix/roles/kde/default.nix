4b4f51ea215e9e3838c2893a0fb368a9526eecf6
Jade Lovelace <software@lfcode.ca>
2023-10-26 13:32:17 -0700

    kde happens

--- configs/nix/roles/kde/default.nix
+++ configs/nix/roles/kde/default.nix
@@ -3,4 +3,6 @@
   services.xserver.desktopManager.plasma5 = {
     enable = true;
   };
+  # conflicts with programs.seahorse
+  programs.ssh.askPassword = "${pkgs.libsForQt5.plasma5.ksshaskpass.out}/bin/ksshaskpass";
 }
