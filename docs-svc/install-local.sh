2e0cd1fcce00ff7c226e7ace55473b4716ddb46c
Jade Lovelace <software@lfcode.ca>
2023-11-08 14:22:16 -0800

    install-script harder

--- docs-svc/install-local.sh
+++ docs-svc/install-local.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-nix-build ./docs-service.nix -o docs-service-result
-ln -sf "$(readlink docs-service-result)"/docs.service ~/.config/systemd/user/docs.service
+thisdir="$(realpath "$(dirname "$0")")"
+
+# install fancyindex styling
+mkdir -p ~/.docs
+ln -sf "$thisdir/.style" ~/.docs/.style
+
+# install symlink to rust docs
+ln -sf ~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/share/doc/rust/html ~/.docs/rust
+
+# install docs.service nginx
+nix-build ./docs-service.nix -o "$thisdir/docs-service-result"
+ln -sf "$(readlink "$thisdir/docs-service-result")"/docs.service ~/.config/systemd/user/docs.service
 systemctl --user daemon-reload
 systemctl --user restart docs.service
