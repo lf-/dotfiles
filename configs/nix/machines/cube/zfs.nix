9227946eb89fc60926d18e153db6e0b38ca032a9
Jade Lovelace <software@lfcode.ca>
2024-01-02 12:23:36 +0100

    maybe this fixes the startup issue

--- configs/nix/machines/cube/zfs.nix
+++ configs/nix/machines/cube/zfs.nix
@@ -1,8 +1,4 @@
 { config, pkgs, ... }: {
-  # old code to make zfs-mount-generator work
-  # but i don't think this is a good way to do it since nixos zfs also does zfs
-  # shit and zfs-mount-generator afaict wants to actually mount things.
-  # environment.etc."zfs/zed.d/history_event-zfs-list-cacher.sh".source = "${config.boot.zfs.package}/etc/zfs/zed.d/history_event-zfs-list-cacher.sh";
   systemd.mounts = [
     {
       description = "snowflake backup target";
@@ -11,7 +7,8 @@
       type = "btrfs";
       options = "nossd";
 
-      requires = [ "zfs-import-tank.service" ];
+      wants = [ "tank-backup.mount" ];
+      after = [ "tank-backup.mount" ];
       wantedBy = [ "local-fs.target" ];
     }
   ];
