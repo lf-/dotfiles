dc949152e96ed0cbdb0c7cb714bb5bcc378f34f8
Jade Lovelace <software@lfcode.ca>
2024-01-04 11:26:36 +0100

    lol lmao

--- configs/nix/machines/cube/hedgedoc.nix
+++ configs/nix/machines/cube/hedgedoc.nix
@@ -2,25 +2,29 @@
 let
   domain = "pad.h.jade.fyi";
   port = 17490;
+  dbPath = "/tank/srv/hedgedoc/db.sqlite";
+  uploadsPath = "/tank/srv/hedgedoc/uploads";
 in
 {
   services.hedgedoc = {
     enable = true;
     settings = {
-      inherit domain port;
+      inherit domain port uploadsPath;
       protocolUseSSL = true;
       db = {
         dialect = "sqlite";
-        storage = "/tank/srv/hedgedoc/db.sqlite";
+        storage = dbPath;
       };
-      uploadsPath = "/tank/srv/hedgedoc/uploads";
       allowGravatar = false;
     };
   };
 
   systemd.tmpfiles.rules = [
     "d /tank/srv/hedgedoc 0770 hedgedoc hedgedoc"
-    "d /tank/srv/hedgedoc/uploads 0770 hedgedoc hedgedoc"
+    # required because *these* are the readwritepaths of the service, not the
+    # parent directory, so it cannot create these files itself.
+    "d ${uploadsPath} 0770 hedgedoc hedgedoc"
+    "f ${dbPath} 0660 hedgedoc hedgedoc"
   ];
 
   jade.caddy-wildcard.hosts.${domain} = ''
