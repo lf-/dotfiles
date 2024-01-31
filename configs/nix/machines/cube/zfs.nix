39320d0e625b06e3c26a5430357fbc69a9144bc2
Jade Lovelace <software@lfcode.ca>
2024-01-31 00:08:49 -0800

    put the snowflake backup target in encrypted storage

--- configs/nix/machines/cube/zfs.nix
+++ configs/nix/machines/cube/zfs.nix
@@ -2,7 +2,7 @@
   systemd.mounts = [
     {
       description = "snowflake backup target";
-      what = "/dev/zvol/tank/backup/snowflake_raw";
+      what = "/dev/zvol/tank/backup/zfs/snowflake_raw";
       where = "/tank/backup/snowflake";
       type = "btrfs";
       options = "nossd";
