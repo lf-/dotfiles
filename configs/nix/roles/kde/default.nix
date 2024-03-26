49130a7322da477ffb01f0f0597dc0f44985f9af
Jade Lovelace <software@lfcode.ca>
2024-03-26 14:17:42 -0700

    kate

--- configs/nix/roles/kde/default.nix
+++ configs/nix/roles/kde/default.nix
@@ -5,4 +5,8 @@
   };
   # conflicts with programs.seahorse
   programs.ssh.askPassword = "${pkgs.libsForQt5.plasma5.ksshaskpass.out}/bin/ksshaskpass";
+
+  environment.systemPackages = with pkgs.libsForQt5; [
+    kate
+  ];
 }
