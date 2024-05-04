90b0fb904d5aea1aef3120ac39c74b316907de24
Jade Lovelace <software@lfcode.ca>
2024-05-03 20:08:00 -0700

    add more shortcuts to local-packages

--- configs/nix/local-packages.nix
+++ configs/nix/local-packages.nix
@@ -1,3 +1,3 @@
 pkgs: {
-  inherit (pkgs) nvimsplit nvremote vim-swapfile-header spr-sunshowers openttd-claire opengist jellyfin-plugin-sso gerrit-oauth-provider clangbuildanalyzer;
+  inherit (pkgs) nvimsplit nvremote vim-swapfile-header spr-sunshowers openttd-claire opengist jellyfin-plugin-sso gerrit-oauth-provider clangbuildanalyzer nix cinny-desktop;
 }
