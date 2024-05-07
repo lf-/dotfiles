518c646fc996b235dbbf170875b7f715eeb6c3b6
Jade Lovelace <software@lfcode.ca>
2024-05-06 18:30:29 -0700

    Update lix now that it is public

--- configs/nix/flake.nix
+++ configs/nix/flake.nix
@@ -52,12 +52,12 @@
     };
 
     lix = {
-      url = "git+ssh://git@lix.systems/lix-project/lix";
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
       flake = false;
     };
 
     lix-module = {
-      url = "git+ssh://git@lix.systems/lix-project/nixos-module";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
       inputs.lix.follows = "lix";
