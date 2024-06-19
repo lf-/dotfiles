e7d940f79fd106e7fffebec48dbd30f1b2a16898
Jade Lovelace <software@lfcode.ca>
2024-06-18 17:00:14 -0700

    nvremote: stop rebuilding on random other changes in the git repo
    Just use an explicit source set that gets recopied. It will work better.

--- programs/nvremote/package.nix
+++ programs/nvremote/package.nix
@@ -1,11 +1,21 @@
-{ rustPlatform, libiconv, pkg-config, gitignoreSource }:
+{ lib, rustPlatform, libiconv, pkg-config, gitignoreSource }:
 rustPlatform.buildRustPackage {
   pname   = "nvremote";
   version = "0.0.0";
 
   cargoLock.lockFile = ./Cargo.lock;
 
-  src = gitignoreSource ../.;
+  src = lib.fileset.toSource {
+    root = ../.;
+    fileset = lib.fileset.unions [
+      ../vim-swapfile-header/src
+      ../vim-swapfile-header/Cargo.toml
+      ../vim-swapfile-header/Cargo.lock
+      ./Cargo.toml
+      ./Cargo.lock
+      ./src
+    ];
+  };
   sourceRoot = "source/nvremote";
 
   nativeBuildInputs = [
