2c9b1e516f290460318551a90bdb321f26d1a21a
Joel Pepper <joel.pepper@colenio.com>
2024-09-21 20:00:11 +0200

    refactor: change internal naming from fpath sync to completion sync

--- zsh-completion-sync.plugin.zsh
+++ zsh-completion-sync.plugin.zsh
@@ -1,21 +1,21 @@
-_fpath_sync:debug_log(){
+_completion_sync:debug_log(){
   if zstyle -t "$1" debug; then
-    echo "FPATH_SYNC: $2"
+    echo "completion sync: $2"
   fi
 }
 
-_fpath_sync:delete_first_from_fpath(){
+_completion_sync:delete_first_from_fpath(){
   # There might be multiple instances of the same path on the fpath
   # We will only delete the first instance to "reset" the priority order
   # but leave later occurences
   local idx="$fpath[(Ie)$1]"
   if (( $idx != 0 )); then
-    _fpath_sync:debug_log ':completion-sync:fpath:delete' "deleting '$1' from FPATH at index '$idx'"
+    _completion_sync:debug_log ':completion-sync:fpath:delete' "deleting '$1' from FPATH at index '$idx'"
     fpath[$idx]=()
   fi
 }
 
-_fpath_sync:fpath_maybe_add_xdg(){
+_completion_sync:fpath_maybe_add_xdg(){
   # There are two valid paths in an XDG_DATA_DIR, one from the zsh install and one from third party
   # It is unclear if would ever make sense to add the ones from the zsh install,
   # since they should be always on the fpath, but for now we test for both
@@ -23,59 +23,59 @@ _fpath_sync:fpath_maybe_add_xdg(){
   local p="$1/zsh/$ZSH_VERSION/functions"
 
   if [[ -d $p ]]; then
-    _fpath_sync:debug_log ':completion-sync:xdg:add' "Added '$p' to FPATH"
+    _completion_sync:debug_log ':completion-sync:xdg:add' "Added '$p' to FPATH"
     fpath=("$p" $fpath)
   fi
 
 
   p="$1/zsh/site-functions"
   if [[ -d $p ]]; then
-    _fpath_sync:debug_log ':completion-sync:xdg:add' "Added '$p' to FPATH"
+    _completion_sync:debug_log ':completion-sync:xdg:add' "Added '$p' to FPATH"
     fpath=("$p" $fpath)
   fi
 }
 
-_fpath_sync:functions_from_xdg_data(){
+_completion_sync:functions_from_xdg_data(){
   local a=($(echo "$XDG_DATA_DIRS" | tr ':' "\n" | xargs -I{} realpath -e "{}/zsh/site-functions" "\n" realpath -e "{}/zsh/$ZSH_VERSION/functions" 2>/dev/null | tr "\n" ' '))
   # unique the directories
   echo "${(u)a[@]}"
 }
 
-_fpath_sync:hook(){
-  if [[ ! -v FPATH_SYNC_OLD_XDG_DATA_DIRS ]] then
-    _fpath_sync:debug_log ':completion-sync:xdg:init' "Syncing XDG_DATA_DIRS into FPATH enabled"
+_completion_sync:hook(){
+  if [[ ! -v COMPLETION_SYNC_OLD_XDG_DATA_DIRS ]] then
+    _completion_sync:debug_log ':completion-sync:xdg:init' "Syncing XDG_DATA_DIRS into FPATH enabled"
 
-    _fpath_sync:debug_log ':completion-sync:xdg:init:diff' "old FPATH\n${(F)fpath}"
+    _completion_sync:debug_log ':completion-sync:xdg:init:diff' "old FPATH\n${(F)fpath}"
 
     # First time around, only add relevant XDG_DATA_DIRs, which are not on the FPATH yet
     # Find XDG_DATA_DIRS which have $ZSH function dirs under them
-    fpath_sync_old_xdg_fpaths=( $(_fpath_sync:functions_from_xdg_data) )
+    completion_sync_old_xdg_fpaths=( $(_completion_sync:functions_from_xdg_data) )
 
-    _fpath_sync:debug_log ':completion-sync:xdg:init:diff' "adding from XDG"
+    _completion_sync:debug_log ':completion-sync:xdg:init:diff' "adding from XDG"
 
     # Prepend in reverse order to maintain their order in the final path
-    for idx in {${#fpath_sync_old_xdg_fpaths}..1} ; do
-      local elem="${fpath_sync_old_xdg_fpaths[$idx]}"
+    for idx in {${#completion_sync_old_xdg_fpaths}..1} ; do
+      local elem="${completion_sync_old_xdg_fpaths[$idx]}"
       if (( ! ${fpath[(I)"$elem"]} )); then
 
-        _fpath_sync:debug_log ':completion-sync:xdg:init:diff' $elem
+        _completion_sync:debug_log ':completion-sync:xdg:init:diff' $elem
 
         fpath=($elem $fpath)
       fi
     done
 
-    _fpath_sync:debug_log ':completion-sync:xdg:init:diff' "New FPATH\n${(F)fpath}"
+    _completion_sync:debug_log ':completion-sync:xdg:init:diff' "New FPATH\n${(F)fpath}"
 
-  elif [[ "$FPATH_SYNC_OLD_XDG_DATA_DIRS" != "$XDG_DATA_DIRS" ]]; then
-    _fpath_sync:debug_log ':completion-sync:xdg:onchange' "XDG_DATA_DIRS CHANGED"
+  elif [[ "$COMPLETION_SYNC_OLD_XDG_DATA_DIRS" != "$XDG_DATA_DIRS" ]]; then
+    _completion_sync:debug_log ':completion-sync:xdg:onchange' "XDG_DATA_DIRS CHANGED"
     # Check if the fpath dirs changed
-    local new_paths=( $(_fpath_sync:functions_from_xdg_data) )
+    local new_paths=( $(_completion_sync:functions_from_xdg_data) )
 
-    if [[ "$fpath_sync_old_xdg_fpaths" != "$new_paths" ]]; then
-      _fpath_sync:debug_log ':completion-sync:xdg:onchange' "Need to update FPATH from XDG_DATA_DIRS!"
+    if [[ "$completion_sync_old_xdg_fpaths" != "$new_paths" ]]; then
+      _completion_sync:debug_log ':completion-sync:xdg:onchange' "Need to update FPATH from XDG_DATA_DIRS!"
 
-      local diff=( "${(@)$(diff <(for p in $new_paths; do echo $p; done) <(for p in $fpath_sync_old_xdg_fpaths; do echo $p; done) | grep -E "<|>")}" )
-      _fpath_sync:debug_log ':completion-sync:xdg:diff' "$diff"
+      local diff=( "${(@)$(diff <(for p in $new_paths; do echo $p; done) <(for p in $completion_sync_old_xdg_fpaths; do echo $p; done) | grep -E "<|>")}" )
+      _completion_sync:debug_log ':completion-sync:xdg:diff' "$diff"
 
       # Prepend in reverse order to maintain their order in the final path
       for idx in {${#diff}..1} ; do
@@ -84,39 +84,39 @@ _fpath_sync:hook(){
           \<)
             # path got added
             local p_path="${p:2}"
-            _fpath_sync:debug_log ':completion-sync:xdg:onchange:add' "Adding path '$p_path'"
-            _fpath_sync:debug_log ':completion-sync:fpath:add' "Adding '$p_path' to FPATH"
+            _completion_sync:debug_log ':completion-sync:xdg:onchange:add' "Adding path '$p_path'"
+            _completion_sync:debug_log ':completion-sync:fpath:add' "Adding '$p_path' to FPATH"
             fpath=("$p_path" $fpath)
             ;;
           \>)
             # path got removed
             local p_path="${p:2}"
-            _fpath_sync:debug_log ':completion-sync:xdg:onchange:delete' "Removing path '$p_path'"
-            _fpath_sync:delete_first_from_fpath "$p_path"
+            _completion_sync:debug_log ':completion-sync:xdg:onchange:delete' "Removing path '$p_path'"
+            _completion_sync:delete_first_from_fpath "$p_path"
             ;;
           *)
             # This should not happen
-            _fpath_sync:debug_log ':completion-sync:xdg:onchange' "Invalid diff line $p"
-            _fpath_sync:debug_log ':completion-sync:xdg:onchange' "Tried to match on character ${p[1]}"
+            _completion_sync:debug_log ':completion-sync:xdg:onchange' "Invalid diff line $p"
+            _completion_sync:debug_log ':completion-sync:xdg:onchange' "Tried to match on character ${p[1]}"
             ;;
         esac
       done
 
-      fpath_sync_old_xdg_fpaths=( "${(@f)new_paths}" )
+      completion_sync_old_xdg_fpaths=( "${(@f)new_paths}" )
     else
-      _fpath_sync:debug_log ':completion-sync:xdg:onchange' "No FPATH change needed"
+      _completion_sync:debug_log ':completion-sync:xdg:onchange' "No FPATH change needed"
     fi
   fi
-  FPATH_SYNC_OLD_XDG_DATA_DIRS="$XDG_DATA_DIRS"
+  COMPLETION_SYNC_OLD_XDG_DATA_DIRS="$XDG_DATA_DIRS"
 
-  if [[ ! -v fpath_sync_old_fpath ]]; then
-    _fpath_sync:debug_log ':completion-sync:fpath:init' "FPATH_SYNC init"
+  if [[ ! -v completion_sync_old_fpath ]]; then
+    _completion_sync:debug_log ':completion-sync:fpath:init' "Syncing completions to fpath enabled"
     # Do no re-init the first time around
-  elif [[ "$fpath_sync_old_fpath" != "$fpath" ]]; then
+  elif [[ "$completion_sync_old_fpath" != "$fpath" ]]; then
 
-    _fpath_sync:debug_log ':completion-sync:fpath:onchange' "FPATH Changed!"
+    _completion_sync:debug_log ':completion-sync:fpath:onchange' "FPATH Changed!"
     if zstyle -t ':completion-sync:fpath:onchange:diff' debug; then
-      diff <(echo "${(F)fpath}" | sort ) <(echo "${(F)fpath_sync_old_fpath}" | sort) | grep -E "<|>"
+      diff <(echo "${(F)fpath}" | sort ) <(echo "${(F)completion_sync_old_fpath}" | sort) | grep -E "<|>"
     fi
 
     # Allow us to restore the previous compinit, to be a good citizen
@@ -126,29 +126,29 @@ _fpath_sync:hook(){
     # restore original compinit
     autoload +X compinit
 
-    _fpath_sync:debug_log ':completion-sync:compinit:autoload' "previous compinit: $(whence -v compinit_orig)"
-    _fpath_sync:debug_log ':completion-sync:compinit:autoload' "loaded compinit: $(whence -v compinit)"
+    _completion_sync:debug_log ':completion-sync:compinit:autoload' "previous compinit: $(whence -v compinit_orig)"
+    _completion_sync:debug_log ':completion-sync:compinit:autoload' "loaded compinit: $(whence -v compinit)"
 
 
     # do not write dumpfile, since we are likely working on a temporary FPATH
     # TODO: make argument configurable
-    _fpath_sync:debug_log ':completion-sync:compinit' "invoking compinit as 'compinit -D'"
+    _completion_sync:debug_log ':completion-sync:compinit' "invoking compinit as 'compinit -D'"
     compinit -D
     # restore original function
     functions -c compinit_orig compinit
-    _fpath_sync:debug_log ':completion-sync:compinit:autoload' "restored compinit: $(whence -v compinit)"
+    _completion_sync:debug_log ':completion-sync:compinit:autoload' "restored compinit: $(whence -v compinit)"
 
   fi
-  fpath_sync_old_fpath=( "${(@f)fpath}" )
+  completion_sync_old_fpath=( "${(@f)fpath}" )
 }
 
 typeset -ag precmd_functions
-if (( ! ${precmd_functions[(I)_fpath_sync:hook]} )); then
+if (( ! ${precmd_functions[(I)_completion_sync:hook]} )); then
 # Add our hook last to go after _direnv_hook
-  precmd_functions=($precmd_functions _fpath_sync:hook)
+  precmd_functions=($precmd_functions _completion_sync:hook)
 fi
 typeset -ag chpwd_functions
-if (( ! ${chpwd_functions[(I)_fpath_sync:hook]} )); then
+if (( ! ${chpwd_functions[(I)_completion_sync:hook]} )); then
   # Add our hook last to go after _direnv_hook
-  chpwd_functions=($chpwd_functions _fpath_sync:hook)
+  chpwd_functions=($chpwd_functions _completion_sync:hook)
 fi
