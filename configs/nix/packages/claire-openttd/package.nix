fd1294c16b218db9b7f7b523e7c6ea61b9d96ffe
Jade Lovelace <software@lfcode.ca>
2024-11-30 21:19:37 -0800

    update openttd

--- configs/nix/packages/claire-openttd/package.nix
+++ configs/nix/packages/claire-openttd/package.nix
@@ -3,7 +3,7 @@ openttd.overrideAttrs (old: {
   src = fetchFromGitHub {
     owner = "jgrennison";
     repo = "openttd-patches";
-    rev = "jgrpp-0.57.1";
-    sha256 = "sha256-mQy+QdhEXoM9wIWvSkMgRVBXJO1ugXWS3lduccez1PQ=";
+    rev = "jgrpp-0.63.1";
+    sha256 = "sha256-Jg+BBGab23pBREVjiyxbUimKfpSoYxMfNjko02QaHcw=";
   };
 })
