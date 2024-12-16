fca49629ac7f763c7d1db2e4219b774c3a7a159a
Jade Lovelace <software@lfcode.ca>
2024-12-16 15:20:38 -0800

    maybe unbreak ts parsers, use nixfmt

--- configs/nvim/lateinit.lua
+++ configs/nvim/lateinit.lua
@@ -12,7 +12,8 @@ local nvim_exec = vim.api.nvim_exec
 local nvim_call = vim.api.nvim_call_function
 
 require 'nvim-treesitter.configs'.setup {
-    ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = "all",
+    sync_install = false,
     ignore_install = { "scfg", "smali" }, -- List of parsers to ignore installing
     highlight = {
         enable = true,
