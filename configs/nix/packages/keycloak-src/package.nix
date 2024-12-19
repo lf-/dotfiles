427b94288d96a53cb78c89f2b42c5d7824a37bda
Jade Lovelace <software@lfcode.ca>
2024-12-18 23:17:21 -0800

    keycloak: fix package. jar name was wrong lol

--- configs/nix/packages/keycloak-src/package.nix
+++ configs/nix/packages/keycloak-src/package.nix
@@ -1,6 +1,6 @@
 { maven, fetchFromGitHub, keycloak }:
 let
-  version = "26.0.5";
+  version = "26.0.6";
 
   # rebuild services with patches
   services = assert keycloak.version == version; maven.buildMavenPackage {
@@ -11,14 +11,14 @@ let
       owner = "keycloak";
       repo = "keycloak";
       rev = version;
-      hash = "sha256-OnFNaS1o6e3x53h28sygymlQH6RGwl+bqKlPm5i8uu8=";
+      hash = "sha256-UYE25jJJ13Pg+EeKhIE2NojiDrg1fZQO9n/DeAfB/pQ=";
     };
 
     patches = [ ./0001-hack-make-the-WebAuthn-authenticator-accept-binary-U.patch ];
 
     mvnDepsParameters = "-Daether.dependencyCollector.impl=bf";
 
-    mvnHash = "sha256-QeCQr/CNoe7bEdjvEbUE2q50jU5oJow/KQ7jjiGqpNk=";
+    mvnHash = "sha256-/hqO+PrPlkTw4rs4VgO0azig2tk8f2Qr4DBxz3EgRLQ=";
 
     mvnParameters = "-DskipTestsuite -DskipExamples -DskipTests -pl :keycloak-services,:keycloak-core,:keycloak-crypto-default,:keycloak-common,:keycloak-server-spi,:keycloak-server-spi-private,:keycloak-saml-core-public,:keycloak-saml-core,:keycloak-model-storage-private,:keycloak-model-storage,:keycloak-config-api";
 
@@ -32,6 +32,10 @@ let
 in
 keycloak.overrideAttrs (old: {
   preBuild = ''
-    cp -r ${services}/* lib/lib/main/
+    cp ${services}/keycloak-services-${version}.jar lib/lib/main/org.keycloak.keycloak-services-${version}.jar
   '';
+
+  passthru = old.passthru // {
+    services = services;
+  };
 })
