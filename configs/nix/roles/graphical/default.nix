98779126f2045a5e5521fc472591d56a2d08d32d
Jade Lovelace <software@lfcode.ca>
2025-02-28 13:35:09 -0800

    graphical: minimal profile

--- configs/nix/roles/graphical/default.nix
+++ configs/nix/roles/graphical/default.nix
@@ -1,94 +1,129 @@
-{ config, pkgs, lib, ... }:
 {
-  imports = [ ../fonts ../avahi ];
-
-  boot.supportedFilesystems = [ "cifs" ];
-
-  environment.systemPackages = with pkgs; [
-    firefox-wayland
-    chromium
-    alacritty
-    glxinfo
-    yubikey-manager-qt
-    xsel
-    xclip
-    wl-clipboard
-    zoom-us
-    xdg-utils
-    obs-studio
-    kdenlive
-    spotify
-    libreoffice
-    arandr
-    gimp
-    inkscape
-    zeal
-    signal-desktop
-    zotero
-    element-desktop
-
-    alsa-utils
-    qpwgraph
-
-    kooha
-    # FIXME: kooha doesn't work on my bspwm machine
-    peek
-
-    pdfarranger
-    pdfgrep
-    # FIXME: jpdftweak
-    qpdf
-
-    vlc
-    mpv
-
-    meld
-
-    d-spy
-
-    vscode
-
-    prusa-slicer
-
-    pulseaudio
-    pavucontrol
-
-    audacity
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  cfg = config.jade.graphical;
+in
+{
+  imports = [
+    ../fonts
+    ../avahi
   ];
 
-  users.users.jade.extraGroups = [ "rtkit" ];
-
-  nixpkgs.config.firefox.speechSynthesisSupport = true;
-  services.printing.enable = true;
-  services.printing.drivers = with pkgs; [ hplip ];
-  services.avahi.enable = true;
-
-  services.udisks2.enable = true;
-
-  programs.kdeconnect.enable = true;
-
-  # deal with the dbus systemd interaction bug where it will not fail things if
-  # they fail in systemd
-  services.dbus.implementation = "broker";
-
-  # gitk
-  programs.git.package = pkgs.gitFull;
-
-  services.pulseaudio.enable = lib.mkForce false;
-
-  services.pipewire = {
-    enable = true;
-    audio.enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+  options.jade.graphical = {
+    minimal = lib.mkOption {
+      description = "Minimal setup, few programs installed. Intended for when you need a GUI for strange compliance purposes.";
+      type = lib.types.bool;
+      default = false;
+    };
   };
-  security.rtkit.enable = true;
 
-  programs.dconf.enable = true;
-  hardware.bluetooth.enable = true;
-  hardware.acpilight.enable = true;
+  config = lib.mkMerge [
+    (lib.mkIf (!cfg.minimal) {
+      boot.supportedFilesystems = [ "cifs" ];
 
-  boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
-  boot.kernelModules = [ "v4l2loopback" ];
+      environment.systemPackages = with pkgs; [
+        yubikey-manager-qt
+        zoom-us
+
+        # editors
+        kdenlive
+        spotify
+        libreoffice
+        gimp
+        inkscape
+        audacity
+
+        # document shuffling
+        zeal
+        zotero
+
+        # communication
+        signal-desktop
+        element-desktop
+
+        # screen recording
+        kooha
+        # FIXME: kooha doesn't work on my bspwm machine
+        peek
+        obs-studio
+
+        # pdfs
+        pdfarranger
+        pdfgrep
+        # FIXME: jpdftweak
+        qpdf
+
+        # video viewers
+        vlc
+        mpv
+
+        # dev tools
+        meld
+        d-spy
+        vscode
+
+        # threedy printers
+        prusa-slicer
+      ];
+
+      nixpkgs.config.firefox.speechSynthesisSupport = true;
+      services.printing.enable = true;
+      services.printing.drivers = with pkgs; [ hplip ];
+      services.avahi.enable = true;
+
+      programs.kdeconnect.enable = true;
+
+      # gitk
+      programs.git.package = pkgs.gitFull;
+
+      boot.extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+      boot.kernelModules = [ "v4l2loopback" ];
+    })
+    {
+      environment.systemPackages = with pkgs; [
+        firefox-wayland
+        chromium
+        alacritty
+        glxinfo
+        xsel
+        xclip
+        wl-clipboard
+        xdg-utils
+        arandr
+
+        alsa-utils
+        qpwgraph
+
+        pulseaudio
+        pavucontrol
+      ];
+
+      users.users.jade.extraGroups = [ "rtkit" ];
+
+      services.udisks2.enable = true;
+
+      # deal with the dbus systemd interaction bug where it will not fail things if
+      # they fail in systemd
+      services.dbus.implementation = "broker";
+
+      services.pulseaudio.enable = lib.mkForce false;
+
+      services.pipewire = {
+        enable = true;
+        audio.enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+      };
+      security.rtkit.enable = true;
+
+      programs.dconf.enable = true;
+      hardware.bluetooth.enable = true;
+      hardware.acpilight.enable = true;
+    }
+  ];
 }
