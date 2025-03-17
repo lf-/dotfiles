ee2573fc4fd5c36cbce7196f84a35bf186e5cd0e
Jade Lovelace <software@lfcode.ca>
2025-03-17 11:00:40 -0700

    update lix

--- configs/nix/overlays/jadeware.nix
+++ configs/nix/overlays/jadeware.nix
@@ -2,14 +2,4 @@ final: prev: {
   vim-swapfile-header = final.callPackage ../../../programs/vim-swapfile-header/package.nix { };
   nvimsplit = final.callPackage ../../../programs/nvimsplit/package.nix { };
   nvremote = final.callPackage ../../../programs/nvremote/package.nix { };
-
-  # FIXME: https://github.com/NixOS/nixpkgs/pull/385865
-  git-revise = prev.git-revise.overrideAttrs (old: {
-    disabledTests =
-      assert !(old ? disabledTests);
-      prev.lib.optionals final.stdenv.hostPlatform.isDarwin [
-        # `gpg: agent_genkey failed: No agent running`
-        "test_gpgsign"
-      ];
-  });
 }
