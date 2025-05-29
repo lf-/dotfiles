d90e99fbf04bfbca2fb9101df1e289fcb4b84135
Jade Lovelace <software@lfcode.ca>
2025-05-28 18:30:59 -0700

    fix: octoprint breakage

--- configs/nix/machines/cube/octoprint.nix
+++ configs/nix/machines/cube/octoprint.nix
@@ -1,19 +1,41 @@
-{ ... }:
+{ config, ... }:
 let
   port = 11062;
-  domain = "octoprint.h.jade.fyi";
 in
 {
   services.octoprint = {
     enable = true;
-    host = "127.0.0.1";
+    # FIXME: uncomment after https://github.com/NixOS/nixpkgs/pull/411942
+    # host = "127.0.0.1";
     inherit port;
   };
 
-  jade.caddy-wildcard.hosts.${domain} = {
-    action = ''
-      reverse_proxy localhost:${toString port}
-    '';
-    publicAccess = false;
+  jade.caddy-wildcard.hosts = {
+    "octoprint.h.jade.fyi" = {
+      action = ''
+        handle /_oauth2_proxy/* {
+          reverse_proxy ${config.services.oauth2-proxy.httpAddress}
+        }
+        handle {
+          forward_auth ${config.services.oauth2-proxy.httpAddress} {
+            uri /_oauth2_proxy/auth
+            # copy_headers Authorization
+
+            @bad status 4xx
+            handle_response @bad {
+              redir * /_oauth2_proxy/start
+            }
+          }
+          reverse_proxy 127.0.0.1:${toString port}
+        }
+      '';
+      publicAccess = true;
+    };
+    "octoprint-ts.h.jade.fyi" = {
+      action = ''
+        reverse_proxy localhost:${toString port}
+      '';
+      publicAccess = false;
+    };
   };
 }
