c2238b072a6350c753207e47ab069896815faf2f
Jade Lovelace <software@lfcode.ca>
2025-07-07 18:06:41 -0700

    polkadots: fix build

--- configs/nix/overlays/polkadots.nix
+++ configs/nix/overlays/polkadots.nix
@@ -1,11 +1,13 @@
 { polkadots }:
 final: curr:
 {
-  polkadots = curr.python3Packages.buildPythonApplication {
+  polkadots = final.python3Packages.buildPythonApplication {
     pname = "polkadots";
     version = "0.0.0";
     doCheck = false;
     src = polkadots;
-    pyproject = false;
+    # no pyproject file but nonetheless it will assume setuptools then work
+    pyproject = true;
+    build-system = [ final.python3Packages.setuptools ];
   };
 }
