d0645c1ca526ae23903310a47e735852805e7daf
Jade Lovelace <software@lfcode.ca>
2025-10-15 14:40:36 -0700

    copybara: fix

--- ci/copybara.sh
+++ ci/copybara.sh
@@ -3,4 +3,31 @@
 set -euo pipefail
 set -x
 
-copybara -v ./vim-plugins/copy.bara.sky buck2_nvim_to_github refs/heads/main
+readonly SUCCESS=0
+# shellcheck disable=SC2034
+readonly COMMAND_LINE_ERROR=1
+# shellcheck disable=SC2034
+readonly CONFIGURATION_ERROR=2
+# shellcheck disable=SC2034
+readonly REPOSITORY_ERROR=3
+readonly NO_OP=4
+# shellcheck disable=SC2034
+readonly INTERRUPTED=8
+# shellcheck disable=SC2034
+readonly ENVIRONMENT_ERROR=30
+# shellcheck disable=SC2034
+readonly INTERNAL_ERROR=31
+
+copybara -v ./vim-plugins/copy.bara.sky buck2_nvim_to_github refs/heads/main && ec=$? || ec=$?
+case $ec in
+    "$NO_OP")
+        echo "No-op. Okay!" >&2
+        ;;
+    "$SUCCESS")
+        echo "Okay!" >&2
+        ;;
+    *)
+        echo "Unexpected Copybara exit status: $ec" >&2
+        exit 1
+        ;;
+esac
