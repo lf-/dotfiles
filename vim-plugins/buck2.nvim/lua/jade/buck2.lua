da14e74ca9dbe296245426a69c116c5ea2e07c49
Rebecca Turner <rbt@sent.as>
2025-11-04 11:15:11 -0800

    buck2.nvim: annotate types

--- vim-plugins/buck2.nvim/lua/jade/buck2.lua
+++ vim-plugins/buck2.nvim/lua/jade/buck2.lua
@@ -2,7 +2,8 @@ local Job = require("plenary.job")
 local M = {}
 
 --- @param cmd table<string>
---- @param on_result any completion callback
+--- @param on_result? fun(job: Job) completion callback
+--- @return Job
 function M.buck2(cmd, on_result)
 	local new_cmd = { "-v0", unpack(cmd) }
 	return Job:new({
@@ -29,7 +30,10 @@ end
 
 --- Runs a uquery command with the given query.
 --- @param query string
+--- @return string[], number
 function M.uquery(query)
+	-- Note: We pass `enable_recording = true`, so we're always going to get
+	-- output here instead of `nil`.
 	return M.buck2({ "uquery", "--console", "simple", "--", query }):sync(4000)
 end
 
