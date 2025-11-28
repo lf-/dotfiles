ef4d20fbff5bad765693065b1c7463d19343f6cb
Jade Lovelace <software@lfcode.ca>
2025-11-27 17:31:30 -0800

    gooeyd: automatic startup with sshc
    Uses a random socket, so that multiple instances can coexist.

--- programs/gooeyd/gooeyd.py
+++ programs/gooeyd/gooeyd.py
@@ -19,22 +19,15 @@ import socket
 from socketserver import TCPServer
 import sys
 import tempfile
-from typing import Tuple
+from typing import Any, Tuple
+import random
 import urllib.parse
 import webbrowser
 
-import pyperclip
-
 TEMPDIR = Path(tempfile.gettempdir())
-SOCK = TEMPDIR / 'gooeyd.sock'
-
-atexit.register(lambda: SOCK.unlink(missing_ok=True))
-SOCK.unlink(missing_ok=True)
+SOCK = TEMPDIR / "gooeyd.sock"
 
 DO_OPEN = True
-
-(copy, paste) = pyperclip.determine_clipboard()
-HAVE_PRIMARY = 'primary' in inspect.signature(copy).parameters
 
 
 # hack from https://stackoverflow.com/questions/21650370/setting-up-an-http-server-that-listens-over-a-file-socket
@@ -51,6 +44,18 @@ class UnixHTTPServer(HTTPServer):
         return (request, ["local", 0])
 
 
+class GooeydServer(UnixHTTPServer):
+    def __init__(
+        self, server_address, RequestHandlerClass, bind_and_activate: bool = True
+    ) -> None:
+        import pyperclip
+
+        (self._copy, self._paste) = pyperclip.determine_clipboard()
+        self.have_primary = "primary" in inspect.signature(self._copy).parameters
+
+        super().__init__(server_address, RequestHandlerClass, bind_and_activate)
+
+
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
@@ -60,22 +65,27 @@ class Reply:
     code: int
     headers: dict[str, str]
     content: str
-    content_type: str = 'application/octet-stream'
+    content_type: str = "application/octet-stream"
 
 
 def parse_path(path) -> Tuple[str, dict[str, list[str]]]:
-    res = urllib.parse.urlparse(path, 'http://')
+    res = urllib.parse.urlparse(path, "http://")
     return (res.path, urllib.parse.parse_qs(res.query))
 
 
 class Handler(BaseHTTPRequestHandler):
+    server: GooeydServer  # type: ignore
+
+    def log_message(self, format: str, *args: Any) -> None:
+        # don't log anything :)
+        pass
 
     def reply(self, reply: Reply):
-        content = reply.content.encode('utf-8')
+        content = reply.content.encode("utf-8")
 
         self.send_response(reply.code)
-        self.send_header('Content-Length', str(len(content)))
-        for (h, v) in reply.headers.items():
+        self.send_header("Content-Length", str(len(content)))
+        for h, v in reply.headers.items():
             self.send_header(h, v)
 
         self.end_headers()
@@ -87,84 +97,114 @@ class Handler(BaseHTTPRequestHandler):
         if handler:
             reply = handler(args=args, body=body)
         else:
-            reply = Reply(404, {}, 'Path not found')
+            reply = Reply(404, {}, "Path not found")
 
         self.reply(reply)
 
     def do_GET(self):
         HANDLERS = {
-            '/paste': self.paste,
+            "/paste": self.paste,
         }
 
         self.call_handler(HANDLERS)
 
     def paste(self, args, **_) -> Reply:
-        if args.get('primary') and HAVE_PRIMARY:
-            clip = paste(primary=True)  # type: ignore
+        if args.get("primary") and self.server.have_primary:
+            clip = self.server._paste(primary=True)  # type: ignore
         else:
-            clip = paste()
+            clip = self.server._paste()
 
         return Reply(code=200, headers={}, content=clip)
 
     def copy(self, body: str, args, **_) -> Reply:
-        if args.get('primary') and HAVE_PRIMARY:
-            copy(body, primary=True)  # type: ignore
+        if args.get("primary") and self.server.have_primary:
+            self.server._copy(body, primary=True)  # type: ignore
         else:
-            copy(body)
+            self.server._copy(body)
 
-        return Reply(code=200, headers={}, content='')
+        return Reply(code=200, headers={}, content="")
 
     def open_url(self, body: str, **_) -> Reply:
         if not DO_OPEN:
-            return Reply(code=401,
-                         headers={},
-                         content='open-url is disabled on this server')
+            return Reply(
+                code=401, headers={}, content="open-url is disabled on this server"
+            )
         # XXX: this is an arbitrary remote command execution on platforms where
         # the browser is set to something like the `open` command. i don't
         # think there's much we can actually do about this short of a highly
         # bypassable filter, however.
 
         browser = webbrowser.get()
-        if not ((isinstance(browser, webbrowser.UnixBrowser)
-                 and browser.background)
-                or isinstance(browser, webbrowser.BackgroundBrowser)):
+        if not (
+            (isinstance(browser, webbrowser.UnixBrowser) and browser.background)
+            or isinstance(browser, webbrowser.BackgroundBrowser)
+        ):
             eprint(
-                f'browser {browser!r} does not support background usage! not doing anything'
+                f"browser {browser!r} does not support background usage! not doing anything"
             )
 
         browser.open(body.strip())
-        return Reply(code=200, headers={}, content='')
+        return Reply(code=200, headers={}, content="")
 
     def do_POST(self):
         HANDLERS = {
-            '/copy': self.copy,
-            '/open-url': self.open_url,
+            "/copy": self.copy,
+            "/open-url": self.open_url,
         }
-        content_len = int(self.headers.get('Content-Length'))
-        post_body = self.rfile.read(content_len).decode('utf-8')
+        content_len = int(self.headers.get("Content-Length", "0"))
+        post_body = self.rfile.read(content_len).decode("utf-8")
 
         self.call_handler(HANDLERS, post_body)
 
 
+def is_socket_listening(path: Path) -> bool:
+    try:
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sock.connect(str(path))
+        sock.close()
+        return True
+    except (FileNotFoundError, ConnectionRefusedError, PermissionError):
+        return False
+
+
 def main(raw_args, prog_name):
     global DO_OPEN
+    global SOCK
     ap = argparse.ArgumentParser(prog=prog_name)
     ap.add_argument(
-        '--disable-open',
-        help='disables the open handler, as it '
-        'may be a remote code execution by opening files for anyone with '
+        "--disable-open",
+        help="disables the open handler, as it "
+        "may be a remote code execution by opening files for anyone with "
         'the socket if your system has "open" or similar set as the '
-        'browser',
-        action='store_true',
-        default=False)
+        "browser",
+        action="store_true",
+        default=False,
+    )
+    ap.add_argument(
+        "--random-socket",
+        help="Use a random file name for the socket. Useful for gooeyd that is run alongside ssh.",
+        action="store_true",
+        default=False,
+    )
 
     args = ap.parse_args(raw_args)
     DO_OPEN = not args.disable_open
+    if args.random_socket:
+        SOCK = SOCK.with_name(SOCK.name + "." + str(random.randint(1, 1_000_000)))
 
-    server = UnixHTTPServer(str(SOCK), Handler)  # type: ignore
-    print(f"export LC_GOOEYD_SOCK={SOCK}")
+    # If it's already running, then just print the path and leave
+    if is_socket_listening(SOCK):
+        eprint("Using existing gooeyd socket")
+        print(f"export LC_GOOEYD_SOCK={SOCK}", flush=True)
+        return
+    else:
+        atexit.register(lambda: SOCK.unlink(missing_ok=True))
+        SOCK.unlink(missing_ok=True)
+
+    server = GooeydServer(str(SOCK), Handler)  # type: ignore
+    print(f"export LC_GOOEYD_SOCK={SOCK}", flush=True)
     server.serve_forever()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv[1:], sys.argv[0])
